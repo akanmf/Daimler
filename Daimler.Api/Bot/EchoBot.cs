dba160acb2353e653418a6bea998a13f18f56b0f
Mehmet Fatih Akan <akanmf@gmail.com>
2019-12-14 10:43:26 +0300

    EchoBot Created

---
+++ Daimler.Api/Bot/EchoBot.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Daimler.Api.Bot
+{
+    public class EchoBot : ActivityHandler
+    {
+        protected async override Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var inputText = turnContext.Activity.Text.Trim().ToLower();
+
+            await turnContext.SendActivityAsync("Echo:" + inputText);
+        }
+    }
+}
---
+++ Daimler.Api/Startup.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Daimler.Api.Bot;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Daimler.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddSingleton<IAdapterIntegration, BotFrameworkHttpAdapter>();
+
+            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.  
+            services.AddTransient<IBot, EchoBot>();
+
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseBotFramework();
+        }
+    }
+}
